8186d3d31f94c96f4973eea848b44a922c04a257
kennytran275 <kennyt275@gmail.com>
2021-03-09 20:36:14 -0500

    added 3 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+// logo heading
+const logoHeading = document.querySelector(".logo-heading");
+
+logoHeading.addEventListener("mouseover", function (event) {
+  event.target.style.backgroundColor = "blue";
+});
+
+logoHeading.addEventListener("mouseout", function (event) {
+  event.target.style.backgroundColor = "white";
+});
+
+// images
+const imgs = document.querySelector("img");
+
+imgs.addEventListener("dblclick", function (event) {
+  event.target.style.border = "thick dotted purple";
+});
+
+//
